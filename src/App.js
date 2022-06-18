fdba64a2b364e368cf6cb04b208ad62d7d2ea68a
king <miabiyewisdom@gmail.com>
2022-06-17 19:56:04 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import React from 'react'
 function App() {
 
   const[advice, setAdvice] = React.useState([]);
-
-
-  
-    const fetchData = async () => {
-      const response = await fetch('https://api.adviceslip.com/advice');
-      const adviceData = await response.json();
-      setAdvice(adviceData);
-      console.log(adviceData)
-      }  
-   React.useEffect(() => {
+  const fetchData = async () => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const adviceData = await response.json();
+    setAdvice(adviceData.slip);
+    console.log(adviceData.slip)
+  }
+  React.useEffect(() => {
+    
      fetchData()
    }, [])
    
@@ -21,11 +19,17 @@ function App() {
   return (
     <div>
       <div className="card">
-        <div className="advice-number">ADVICE </div>
-        <div className="quote">"</div>
-        <div className="border"></div>
-        <button onClick={fetchData()}>dice</button>
+        <div className="advice-number">ADVICE #{advice.id}</div>
+        <div className="quote">"{advice.advice}"</div>
+        <div className="border"><svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+          <g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/>
+          <g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg></div>
       </div>
+      <button onClick={fetchData} className="dice">
+      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 
+      4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 
+      1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+      </button>
     </div>
   );
 }
